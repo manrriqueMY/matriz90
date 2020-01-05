ba0c6c405d3fa555dee44bee765ffc43b5107115
manrriqueMY <manrrique.my@gmail.com>
2020-01-04 22:46:56 -0500

    s

--- resources/views/matriz.blade.php
+++ resources/views/matriz.blade.php
@@ -2,7 +2,7 @@
 	@foreach($ma as $m)
 	<div class="row">
 		@foreach($m as $ij)
-			<div class="col-md-1">{{$ij}}</div>
+			<div style="width: 40">{{$ij}}</div>
 		@endforeach
 	</div>
 	@endforeach
